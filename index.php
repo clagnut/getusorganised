65c250a29c5f62960454978ac93c72fafe8aeae6
Richard Rutter <rich@clearleft.com>
2014-10-03 17:15:13 +0100

    form items php friendly

--- index.php
+++ index.php
@@ -15,41 +15,41 @@ include($dr . "header.php");
 
 <p>You’re here because you need to get a bunch of people to agree on an activity, a place and/or a time.</p>
 
-<form action="save.php">
+<form action="save.php" method="post">
 
 <label for="title">What are you trying to organise?
 <input type="text" id="title" /> <small>eg. James's Stag Do</small></label>
 
 
 <fieldset>
-<legend><strong>What</strong> are you doing?</legend>
-<label for="what1">Option 1
-<input type="text" id="what1" /> <small>leave empty if you don't need to agree this</small></label>
-<label for="what2">Option 2
-<input type="text" id="what2" /></label>
-<label for="what3">Option 3
-<input type="text" id="what3" /></label>
+<legend><strong>What</strong> might you do?</legend>
+<label>Option 1
+<input type="text" id="what[1]" /> <small>leave empty if you don't need to agree this</small></label>
+<label>Option 2
+<input type="text" id="what[2]" /></label>
+<label>Option 3
+<input type="text" id="what[3]" /></label>
 </fieldset>
 
 <fieldset>
-<legend><strong>Where</strong> are you going?</legend>
-<label for="where1">Option 1
-<input type="text" id="where1" /> <small>leave empty if you don't need to agree this</small></label>
-<label for="where2">Option 2
-<input type="text" id="where2" /></label>
-<label for="where3">Option 3
-<input type="text" id="where3" /></label>
+<legend><strong>Where</strong> might you go?</legend>
+<label>Option 1
+<input type="text" id="where[1]" /> <small>leave empty if you don't need to agree this</small></label>
+<label>Option 2
+<input type="text" id="where[2]" /></label>
+<label>Option 3
+<input type="text" id="where[3]" /></label>
 
 </fieldset>
 
 <fieldset>
-<legend><strong>When</strong> is it happening?</legend>
-<label for="date1">Option 1
-<input type="date" id="date1" placeholder="dd/mm/yyyy" /> <input type="time" id="time1" placeholder="hh:mm" /> <small>leave either empty if you don't need to agree this</small></label>
-<label for="date2">Option 2
-<input type="date" id="date2" placeholder="dd/mm/yyyy" /> <input type="time" id="time1" placeholder="hh:mm" /></label>
-<label for="date3">Option 3
-<input type="date" id="date3" placeholder="dd/mm/yyyy" /> <input type="time" id="time1" placeholder="hh:mm" /></label>
+<legend><strong>When</strong> might it happen?</legend>
+<label>Option 1
+<input type="date" id="date[1]" placeholder="dd/mm/yyyy" /> <input type="time" id="time[1]" placeholder="hh:mm" /> <small>leave either empty if you don't need to agree this</small></label>
+<label>Option 2
+<input type="date" id="date[2]" placeholder="dd/mm/yyyy" /> <input type="time" id="time[2]" placeholder="hh:mm" /></label>
+<label>Option 3
+<input type="date" id="date[3]" placeholder="dd/mm/yyyy" /> <input type="time" id="time[3]" placeholder="hh:mm" /></label>
 
 </fieldset>
 
